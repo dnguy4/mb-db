56f8cab33acb23339c37dbde83db2c220a1694c4
David Nguyen <nguyen.david324@gmail.com>
2025-06-15 23:49:57 -0500

    add pelican

---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+AUTHOR = "soup"
+SITENAME = "Millenium Blades Wiki"
+SITEURL = ""
+
+PLUGIN_PATHS = [
+    "../pelican-plugins",
+]
+PLUGINS = ["i18n_subsites", "search"]
+JINJA_ENVIRONMENT = {
+    "extensions": ["jinja2.ext.i18n"],
+}
+
+PATH = "content"
+PAGE_PATHS = ["pages"]
+STATIC_PATHS = ["cards"]
+
+TIMEZONE = "America/Belize"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (("Github", "https://www.github.com/dnguy4"),)
+
+DEFAULT_PAGINATION = False
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# RELATIVE_URLS = True
+THEME = "./pelican-fh5co-marble"
